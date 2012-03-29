1025eaa4df00e727fd8f6d4709848e14f86f745c
Jan Javorek <honza@javorek.net>
2012-03-29 16:41:47 +0200

    removed support for Gravatar by Flask-Gravatar, this will be done in a different, more general way

--- oleander/templating.py
+++ oleander/templating.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
-from flask.ext.gravatar import Gravatar
+#from flask.ext.gravatar import Gravatar
 from oleander import app
 
 
-gravatar = Gravatar(app, size=50, rating='g', default='mm')
+#gravatar = Gravatar(app, size=50, rating='g', default='mm')
 
 
 @app.template_filter
 def avatar(contact):
+    pass
     #if contact.type == 'facebook':
         #return 'https://graph.facebook.com/' + contact.facebook_id + '/picture?type=square'
     #elif contact.type = 'google':
         # http://motyar.blogspot.com/2011/09/fetch-google-plus-profile-picture-using.html
     #else:
-    return gravatar(contact.email)
+    #return gravatar(contact.email)
 
 
