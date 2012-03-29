aaf786a91df0f7d2f85e9c741213236ae8c6ad3f
Jan Javorek <honza@javorek.net>
2012-03-29 15:27:44 +0200

    initial contacts page integration

--- oleander/views/__init__.py
+++ oleander/views/__init__.py
@@ -4,3 +4,4 @@
 from .index import index
 from .auth import sign_up, sign_in, sign_out
 from .settings import settings, change_password
+from .contacts import contacts
---
+++ oleander/views/contacts.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+from flask import render_template
+from flask.ext.login import login_required
+from oleander import app, db
+
+
+@app.route('/contacts/', methods=('GET', 'POST'))
+@login_required
+def contacts():
+    """Contacts management page."""
+    return render_template('contacts.html')
+
+
