f1bf53b352e96ba2af1f67a614d7a135db9d0c50
Jan Javorek <honza@javorek.net>
2012-04-15 22:45:15 +0200

    refinements in the main api, tests

--- oleander/__init__.py
+++ oleander/__init__.py
@@ -4,19 +4,36 @@
 __version__ = '0.0.1'
 
 
+class OleanderException(Exception):
+    """Generic Oleander exception type."""
+    pass
+
+class MissingComponentException(ValueError, OleanderException):
+    """Adapter or mapper of demanded name couldn't be found."""
+    pass
+
+class MissingMethodException(NotImplementedError, OleanderException):
+    """Adapter or mapper doesn't implement demanded type of communitation."""
+    pass
+
+
 class Oleander(object):
+    """Main controller for agnostic communitation with services."""
 
     def __init__(self):
         self._mappers = {}
         self._adapters = {}
 
     def register_mapper(self, entity_name, mapper):
+        """Register entity mapper."""
         self._mappers[entity_name] = mapper
 
     def register_adapter(self, service_name, adapter):
+        """Register service adapter."""
         self._adapters[service_name] = adapter
 
-    def _class_from_string(self, import_string):
+    def __class_from_string(self, import_string):
+        """Take import string and return corresponding class object."""
         components = import_string.split('.')
         module_name = '.'.join(components[:-1])
         class_name = components[-1]
@@ -24,32 +41,35 @@ class Oleander(object):
         return getattr(module, class_name)
 
     def _mapper_for_entity(self, entity_name):
+        """Return mapper for given entity name. If registered as string, import it first."""
         try:
-            mapper = self._mappers[entity]
+            mapper = self._mappers[entity_name]
         except KeyError:
-            raise ValueError("There is no mapper registered for an entity named '%s'." % entity)\
+            raise MissingComponentException("There is no mapper registered for an entity named '%s'." % entity_name)\
 
         if isinstance(mapper, basestring):
             # mapper registered by import string, such as 'awesome_module.mappers.SmurfMapper'
-            cls = self._class_from_string(mapper)
+            cls = self.__class_from_string(mapper)
             mapper = cls()
-            self._mappers[entity] = mapper
+            self._mappers[entity_name] = mapper
         return mapper
 
     def _adapter_for_service(self, service_name):
+        """Return adapter for given service name. If registered as string, import it first."""
         try:
-            adapter = self._adapters[service]
+            adapter = self._adapters[service_name]
         except KeyError:
-            raise ValueError("There is no adapter registered for a service named '%s'." % service)
+            raise MissingComponentException("There is no adapter registered for a service named '%s'." % service_name)
 
         if isinstance(adapter, basestring):
             # adapter registered by import string, such as 'awesome_module.adapters.GargamelAdapter'
-            cls = self._class_from_string(adapter)
+            cls = self.__class_from_string(adapter)
             adapter = cls()
-            self._adapters[service] = adapter
+            self._adapters[service_name] = adapter
         return adapter
 
     def _format_mapper_method(self, action, service_name):
+        """Return mapper's method name according to demanded action and service name."""
         if action == 'get':
             return 'from_' + service_name
         elif action == 'send':
@@ -57,42 +77,47 @@ class Oleander(object):
         else:
             raise ValueError("Unknown action %s." % action)
 
-    def _format_adaper_method(self, action, entity_name):
+    def _format_adapter_method(self, action, entity_name):
+        """Return adapter's method name according to demanded action and entity name."""
         if action in ('get', 'send'):
             return action + '_' + entity_name
         else:
             raise ValueError("Unknown action %s." % action)
 
-    def _call_mapper(self, mapper, action, service_name, data=None):
+    def _call_mapper(self, mapper, action, service_name, data):
+        """Map data to entities, from or to service, according to demanded action."""
         method_name = self._format_mapper_method(action, service_name)
         if hasattr(mapper, method_name):
             method = getattr(mapper, method_name)
-            return method() if data is None else method(data)
+            return method(data)
         else:
             class_name = mapper.__class__.__name__
-            raise NotImplementedError(
+            raise MissingMethodException(
                 "Mapper '%s' can't map '%s' request for service '%s'." % (class_name, action, service_name)
             )
 
     def _call_adapter(self, adapter, action, entity_name, data=None):
+        """Exchange entities with service, according to demanded action."""
         method_name = self._format_adapter_method(action, entity_name)
         if hasattr(adapter, method_name):
             method = getattr(adapter, method_name)
             return method() if data is None else method(data)
         else:
             class_name = adapter.__class__.__name__
-            raise NotImplementedError(
+            raise MissingMethodException(
                 "Adapter '%s' can't %s entities of type '%s'." % (class_name, action, entity_name)
             )
 
-    def get(entity_name, service_name):
+    def get(self, entity_name, service_name):
+        """Get entities from service."""
         mapper = self._mapper_for_entity(entity_name)
         adapter = self._adapter_for_service(service_name)
 
         response = self._call_adapter(adapter, 'get', entity_name)
         return self._call_mapper(mapper, 'get', service_name, data=response)
 
-    def send(entity_name, service_name, data=None):
+    def send(self, entity_name, service_name, data=None):
+        """Send entities to service and return response."""
         mapper = self._mapper_for_entity(entity_name)
         adapter = self._adapter_for_service(service_name)
 
--- tests/test_oleander.py
+++ tests/test_oleander.py
@@ -7,16 +7,111 @@ except ImportError:
     import unittest
 
 
-from oleander import Oleander
+from oleander import Oleander, MissingComponentException, MissingMethodException
+
+
+class ContactsMapper(object):
+
+    from_facebook_called = False
+    to_facebook_called = False
+
+    def from_facebook(self, data):
+        self.from_facebook_called = True
+
+    def to_facebook(self, data):
+        self.to_facebook_called = True
+
+
+class EventsMapper(object):
+    pass
+
+
+class FacebookAdapter(object):
+
+    get_contacts_called = False
+    send_contacts_called = False
+
+    def get_contacts(self):
+        self.get_contacts_called = True
+
+    def send_contacts(self, data=None):
+        self.send_contacts_called = True
 
 
 class TestOleander(unittest.TestCase):
 
-    def test_request(self):
-        pass
+    def test_missing_adapter(self):
+        o = Oleander()
+        o.register_mapper('contacts', ContactsMapper())
+        with self.assertRaises(MissingComponentException):
+            o.get('contacts', 'facebook')
 
-    def test_api_for_humans(self):
-        pass
+    def test_missing_mapper(self):
+        o = Oleander()
+        o.register_adapter('facebook', FacebookAdapter())
+        with self.assertRaises(MissingComponentException):
+            o.get('contacts', 'facebook')
+
+    def test_reqister_by_import_string(self):
+        o = Oleander()
+
+        o.register_mapper('contacts', 'test_oleander.ContactsMapper')
+        o.register_adapter('facebook', 'test_oleander.FacebookAdapter')
+        o.get('contacts', 'facebook')
+
+        self.assertTrue(o._mappers['contacts'].from_facebook_called)
+        self.assertTrue(o._adapters['facebook'].get_contacts_called)
+
+    def test_get(self):
+        o = Oleander()
+        m = ContactsMapper()
+        a = FacebookAdapter()
+
+        o.register_mapper('contacts', m)
+        o.register_adapter('facebook', a)
+
+        o.get('contacts', 'facebook')
+        self.assertTrue(m.from_facebook_called)
+        self.assertTrue(a.get_contacts_called)
+
+    def test_send(self):
+        o = Oleander()
+        m = ContactsMapper()
+        a = FacebookAdapter()
+
+        o.register_mapper('contacts', m)
+        o.register_adapter('facebook', a)
+
+        dummy_data = [2, 4, 6, 8, 10]
+        o.send('contacts', 'facebook', data=dummy_data)
+        self.assertTrue(m.to_facebook_called)
+        self.assertTrue(a.send_contacts_called)
+        self.assertTrue(m.from_facebook_called)
+
+    def test_missing_methods(self):
+        o = Oleander()
+        o.register_mapper('events', EventsMapper())
+        o.register_adapter('facebook', FacebookAdapter())
+        with self.assertRaises(MissingMethodException):
+            o.get('events', 'facebook')
+        with self.assertRaises(MissingMethodException):
+            o.send('events', 'facebook')
+
+        dummy_data = [2, 4, 6, 8, 10]
+        with self.assertRaises(MissingMethodException):
+            o.send('events', 'facebook', data=dummy_data)
+
+    def test_documentation(self):
+        o = Oleander()
+
+        # test class
+        self.assertTrue(o.__doc__)
+
+        # test all public methods
+        for prop_name in dir(o):
+            prop = getattr(o, prop_name)
+            if not prop_name.startswith('_') and hasattr(prop, '__call__'):
+                self.assertTrue(prop.__doc__)
 
 
 if __name__ == '__main__':
